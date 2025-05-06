64804a153ed87153f1d3d7bbee8d1264112cfec4
Tejas <tejas.vaity3@gmail.com>
2025-05-06 22:37:49 +0530

    Messages api backend using socket

--- server/index.js
+++ server/index.js
@@ -3,6 +3,8 @@ const cors = require("cors");
 const mongoose = require("mongoose");
 const app = express();
 const socket = require("socket.io");
+const authRoutes = require("./routes/auth");
+const messageRoutes = require("./routes/messages");
 require("dotenv").config();
 app.use(cors());
 app.use(express.json());
@@ -19,7 +21,35 @@ mongoose
     console.log(err.message);
   });
 
+const server = app.listen(process.env.PORT, () =>
+  console.log(`Server started on ${process.env.PORT}`)
+);
 
-  const server = app.listen(process.env.PORT, () =>
-    console.log(`Server started on ${process.env.PORT}`)
-  );
+app.get("/ping", (_req, res) => {
+  return res.json({ msg: "Ping Successful" });
+});
+
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
+
+const io = socket(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    credentials: true,
+  },
+});
+
+global.onlineUsers = new Map();
+io.on("connection", (socket) => {
+  global.chatSocket = socket;
+  socket.on("add-user", (userId) => {
+    onlineUsers.set(userId, socket.id);
+  });
+
+  socket.on("send-msg", (data) => {
+    const sendUserSocket = onlineUsers.get(data.to);
+    if (sendUserSocket) {
+      socket.to(sendUserSocket).emit("msg-recieve", data.msg);
+    }
+  });
+});
